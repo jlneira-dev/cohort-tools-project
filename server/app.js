abfde22a6c8d60a2fcd40222530e10aff70346ca
Atanas Kisyov <kisyov92@gmail.com>
2024-08-26 16:38:53 +0200

    Day 1 - CORS added

--- server/app.js
+++ server/app.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const PORT = 5005;
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 
-const cohorts = require("./cohorts.json")
-const students = require("./students.json")
-
+const cohorts = require("./cohorts.json");
+const students = require("./students.json");
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
-
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
@@ -23,6 +22,12 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(
+  cors({
+    // Add the URLs of allowed origins to this array
+    origin: [`http://localhost:5173`],
+  })
+);
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
